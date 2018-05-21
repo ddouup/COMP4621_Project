c7d7c33102df9a362fc0f04936a1e7df09720863
Daihui DOU <ddou@connect.ust.hk>
2018-05-21 18:02:46 +0800

    update

--- server.c
+++ server.c
@@ -6,8 +6,8 @@
 #include <netinet/in.h>
 #include <time.h>
 
-#define MAXLINE 1000
-#define SERVER_PORT 12345
+#define MAXLINE 4096
+#define SERVER_PORT 80
 #define LISTENNQ 5
 #define MAXTHREAD 5
 
@@ -21,76 +21,76 @@ void* request_func(void *args);
 
 int main(int argc, char **argv)
 {
-        int listenfd, connfd;
-        struct thread_info *tinfo;
+    int listenfd, connfd;
+    struct thread_info *tinfo;
 
-        struct sockaddr_in serv_addr, cli_addr;
-        socklen_t len = sizeof(struct sockaddr_in);
-        char ip_str[INET_ADDRSTRLEN];
-        char wrt_buff[MAXLINE], rcv_buff[MAXLINE];
+    struct sockaddr_in serv_addr, cli_addr;
+    socklen_t len = sizeof(struct sockaddr_in);
+    char ip_str[INET_ADDRSTRLEN];
+    char wrt_buff[MAXLINE], rcv_buff[MAXLINE];
 
     int threads_count = 0;
     pthread_t threads[MAXTHREAD];
 
-        /* initialize server socket */
-        listenfd = socket(AF_INET, SOCK_STREAM, 0); /* SOCK_STREAM : TCP */
-        if (listenfd < 0) {
-                printf("Error: init socket\n");
-                return 0;
-        }
+    /* initialize server socket */
+    listenfd = socket(AF_INET, SOCK_STREAM, 0); /* SOCK_STREAM : TCP */
+    if (listenfd < 0) {
+            printf("Error: init socket\n");
+            return 0;
+    }
 
-        /* initialize server address (IP:port) */
-        memset(&serv_addr, 0, sizeof(serv_addr));
-        serv_addr.sin_family = AF_INET;
-        serv_addr.sin_addr.s_addr = INADDR_ANY; /* IP address: 0.0.0.0 */
-        serv_addr.sin_port = htons(SERVER_PORT); /* port number */
+    /* initialize server address (IP:port) */
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY; /* IP address: 0.0.0.0 */
+    serv_addr.sin_port = htons(SERVER_PORT); /* port number */
 
-        /* bind the socket to the server address */
-        if (bind(listenfd, (struct sockaddr *)&serv_addr, sizeof(struct sockaddr)) < 0) {
-                printf("Error: bind\n");
-                return 0;
-        }
+    /* bind the socket to the server address */
+    if (bind(listenfd, (struct sockaddr *)&serv_addr, sizeof(struct sockaddr)) < 0) {
+            printf("Error: bind\n");
+            return 0;
+    }
 
-        if (listen(listenfd, LISTENNQ) < 0) {
-                printf("Error: listen\n");
-                return 0;
-        }
+    if (listen(listenfd, LISTENNQ) < 0) {
+            printf("Error: listen\n");
+            return 0;
+    }
 
-        /* keep processing incoming requests */
-        while (1) {
-                /* accept an incoming connection from the remote side */
-                connfd = accept(listenfd, (struct sockaddr *)&cli_addr, &len);
-                if (connfd < 0) {
-                        printf("Error: accept\n");
-                        return 0;
-                }
-                memset(&ip_str, 0, sizeof(ip_str));
-                //memset(&wrt_buff, 0, sizeof(wrt_buff));
-                //memset(&rcv_buff, 0, sizeof(rcv_buff));
-
-                /* print client (remote side) address (IP : port) */
-                inet_ntop(AF_INET, &(cli_addr.sin_addr), ip_str, INET_ADDRSTRLEN);
-                printf("Incoming connection from %s : %hu\n", ip_str, ntohs(cli_addr.sin_port));
-
-                //struct thread_info args;
-
-                if (pthread_create(&threads[threads_count], NULL, request_func, (void *)connfd) != 0) {
-                    printf("Error when creating thread %d\n", threads_count);
+    /* keep processing incoming requests */
+    while (1) {
+            /* accept an incoming connection from the remote side */
+            connfd = accept(listenfd, (struct sockaddr *)&cli_addr, &len);
+            if (connfd < 0) {
+                    printf("Error: accept\n");
                     return 0;
-                }
+            }
+            memset(&ip_str, 0, sizeof(ip_str));
+            //memset(&wrt_buff, 0, sizeof(wrt_buff));
+            //memset(&rcv_buff, 0, sizeof(rcv_buff));
 
-                if (++threads_count >= MAXTHREAD) {
-                    break;
-                }
-                
-        }
-        printf("Nax thread number reached, wait for all threads to finish and exit...\n");
-        int i;
-        for (i = 0; i < MAXTHREAD; ++i) {
-            pthread_join(threads[i], NULL);
-        }
+            /* print client (remote side) address (IP : port) */
+            inet_ntop(AF_INET, &(cli_addr.sin_addr), ip_str, INET_ADDRSTRLEN);
+            printf("Incoming connection from %s : %hu\n", ip_str, ntohs(cli_addr.sin_port));
 
-        return 0;
+            //struct thread_info args;
+
+            if (pthread_create(&threads[threads_count], NULL, request_func, (void *)connfd) != 0) {
+                printf("Error when creating thread %d\n", threads_count);
+                return 0;
+            }
+
+            if (++threads_count >= MAXTHREAD) {
+                break;
+            }
+            
+    }
+    printf("Nax thread number reached, wait for all threads to finish and exit...\n");
+    int i;
+    for (i = 0; i < MAXTHREAD; ++i) {
+        pthread_join(threads[i], NULL);
+    }
+
+    return 0;
 }
 
 void* request_func(void *args)
@@ -125,6 +125,11 @@ void* request_func(void *args)
     /* read from file */
     printf("Reading from file '%s' ...\n", filename);
 
+    
+
+
+
+    
     file = fopen(filename, "r");
     if (!file) {
                 snprintf(wrt_buff, sizeof(wrt_buff) - 1, "%s\n", "The file your requested does not exist ...");
